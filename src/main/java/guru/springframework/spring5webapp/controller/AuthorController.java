e90e0c09f35129667705194000379d8cb74b8035
Archit-07 <Architgupta958@gmail.com>
2020-04-23 03:52:51 +0530

    first commit

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthor(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return  "authors";
+    }
+
+}
